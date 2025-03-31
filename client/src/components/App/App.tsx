ad4b92cc208ca0b11739fc71679ecce42fedb4f2
Maksim <uzhas76064@gmail.com>
2025-03-31 17:02:05 +0300

    удалил приватный роут

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,21 +1,12 @@
 import {Route, Routes} from "react-router";
 import Main from "../../pages/Main/Main.tsx";
 import ContactForm from "../../pages/ContactForm/ContactForm.tsx";
-import PrivateRoute from "../PrivateRoute/PrivateRoute.tsx";
-import ContactSuccess from "../../pages/ContactSuccess/ContactSuccess.tsx";
-import {isSuccess} from "../../const.ts";
 
 function App() {
   return (
    <Routes>
        <Route index path="/" element={<Main />}/>
        <Route path={"/contact-us"} element={<ContactForm />}/>
-       <Route path={"/success-contact"} element={
-           <PrivateRoute isSuccess={isSuccess}>
-               <ContactSuccess />
-           </PrivateRoute>
-       }>
-       </Route>
    </Routes>
   )
 }
